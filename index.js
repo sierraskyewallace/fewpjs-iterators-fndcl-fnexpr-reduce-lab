ff286db688127123942c222246076f454c2948fd
sierraskyewallace <sskwallace@gmail.com>
2021-07-28 15:52:03 -0700

    commit tree 0ad3252339d109ed1b62faaa7509b3d9442ab739

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// var = totalBatteries = sum of all batter amounts from batteryBatches array
+var totalBatteries = 0;
+
+// reduce() function 
+// iterate through batteryBatches array and add each value to totalBatteries
+// totalBatteries is the sum of all battery amounts
+totalBatteries = batteryBatches.reduce(function (a, b) {
+  return a + b;
+});
